b1929c9ff93a493a5b011612d7d01fed87b63ac9
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-03-28 17:17:36 +0200

    change file : add main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 14:10:33 by ezanotti          #+#    #+#             */
-/*   Updated: 2023/03/28 16:46:31 by tgiraudo         ###   ########.fr       */
+/*   Updated: 2023/03/28 17:17:22 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,17 @@ int	main(int argc, char **argv)
 		return (1);
 	if (ft_parsing(&args, argv[1]))
 		return (1);
+
+	int i = -1;
+	int j;
+	while (args.map[++i])
+	{
+		j = -1;
+		while (args.map[i][++j])
+			printf("%c", args.map[i][j]);
+		printf("\n");
+	}
+
 	if (ft_create_window(&args))
 		return (1);
 	ft_free_struct(&args);
