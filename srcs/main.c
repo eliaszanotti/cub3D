f62badb203cf6fc9126c237652c794f784ab89d3
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-03-28 17:27:05 +0200

    change file : add log

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,24 @@
 /*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 14:10:33 by ezanotti          #+#    #+#             */
-/*   Updated: 2023/03/28 17:17:22 by elias            ###   ########.fr       */
+/*   Updated: 2023/03/28 17:27:01 by elias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+void	ft_log(char **map)
+{
+	int i = -1;
+	int j;
+	while (map[++i])
+	{
+		j = -1;
+		while (map[i][++j])
+			printf("%c", map[i][j]);
+		printf("\n");
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,17 +35,7 @@ int	main(int argc, char **argv)
 		return (1);
 	if (ft_parsing(&args, argv[1]))
 		return (1);
-
-	int i = -1;
-	int j;
-	while (args.map[++i])
-	{
-		j = -1;
-		while (args.map[i][++j])
-			printf("%c", args.map[i][j]);
-		printf("\n");
-	}
-
+	//ft_log(args.map);
 	if (ft_create_window(&args))
 		return (1);
 	ft_free_struct(&args);
